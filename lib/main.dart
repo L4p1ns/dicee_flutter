c743a3edeb45cb7b8fa38f6ad909c9f183958c05
bayendembaseye <bayendemba.seye@koonda.fr>
2019-07-14 22:13:38 +0000

    Using StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,39 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
+    leftDiceNumber = 2;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 5;
+                });
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () {
+                print('Right button got pressed.');
+              },
+              child: Image.asset(
+                'images/dice1.png',
+              ),
             ),
           ),
         ],
